9d2f39276958b3add6ad3c56a148d6db32cd0df2
Martok <martok@martoks-place.de>
2015-05-31 01:29:26 +0200

    chg: move generated documentation.html to bin/

--- doc/sax.cmd
+++ doc/sax.cmd
@@ -1,3 +1,3 @@
 java -jar saxon9he.jar -s:master.xml -xi:on -xsl:transform_xml.xsl -o:documentation.xml
-php cleanup.php < documentation.xml > documentation.html
+cmd.exe /c "php cleanup.php < documentation.xml > ../bin/documentation.html"
 del documentation.xml
